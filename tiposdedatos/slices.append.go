200ad825b54f591598a526571af85ea940dec312
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-07-25 19:24:10 +0200

    remove comments by println

--- tiposdedatos/slices.append.go
+++ tiposdedatos/slices.append.go
@@ -11,6 +11,7 @@ func main() {
 
 	title := `*     Los Slices - Append, Añadir elementos a un slice         *`
 
+	fmt.Println(headers.Break)
 	fmt.Println(headers.Title)
 	fmt.Println(headers.TitleSide)
 	fmt.Println(headers.TitleSide)
@@ -19,46 +20,60 @@ func main() {
 	fmt.Println(headers.TitleSide)
 	fmt.Println(headers.Title)
 
-	// declaramos el slice
-	x := make([]byte, 4, 10)
-	fmt.Println("Al declarar el slice x: ", x)
+	fmt.Println(headers.Given)
 
-	// inicializamos x
-	// las 'H' con comillas simples son interpretadas por go como el carácter utf8 de la letra entre comillas
-	// por eso nuestro slice es de tipo byte
+	x := make([]byte, 4, 10)
+
+	fmt.Println("Al declarar el slice x := make([]byte, 4, 10) -> x= ", x)
+
+	fmt.Println(headers.When)
+
 	x = []byte{'H', 'O', 'L', 'A'}
-	fmt.Println("Al inicializar x = []byte{'H', 'O', 'L', 'A'},\ntenemos el slice x: ", x)
-	// imprimimos x dándole formato UTF-8
-	fmt.Printf("Slice x en formato UTF-8: %q \n", x)
+
+	fmt.Println("Las 'H' con comillas simples son interpretadas por go como el carácter utf8 de la letra entre comillas")
+	fmt.Println("por eso nuestro slice es de tipo byte")
+
+	fmt.Println("\nAl inicializar x = []byte{'H', 'O', 'L', 'A'}, tenemos el slice x = ", x)
+
+	fmt.Println("\nImprimimos x dándole formato UTF-8:")
+	fmt.Println("fmt.Printf('el slice x en formato UTF-8 = %q', x)")
+	fmt.Printf("el slice x en formato UTF-8 = %q \n", x)
 
 	for i := 0; i < len(x); i++ {
 		fmt.Printf("Slice x[%d]: %q \n", i, x[i])
 	}
 
-	// El Slice x tiene longitud 4 y capacidad 10, si quewmos añadir otro elemento
-	// al slice necesitamos hacer con la funcion append
+	fmt.Println(headers.Then)
 	fmt.Println("El slice x tiene longitud 4 y capaciad 10")
+	fmt.Println("si quewmos añadir otro elemento al slice necesitaremos hacerlo con la funcion append")
+
 	fmt.Println("\nAntes de un append")
 	fmt.Println("capacidad de x: ", cap(x))
 	fmt.Println("longitud de x: ", len(x))
-	fmt.Println(x)
+	fmt.Println("Siendo el slice x = ", x)
 
-	// append añade el/los elementos hasta que la longitud del slice al añadirlos supere la capacidad,
-	// cuando esto suceda, append, duplicará la capacidad del sice al doble de la longitud que teniamos
-	// antes de proceder al append.
+	fmt.Println(headers.Given)
+	fmt.Println("Declarando un slice 'var y []int' sin capacidad ni longitud")
 
-	// delcaramos el slice sin capacidad ni longitud
 	var y []int
 
-	for i := 1; i < len(y); i++ {
+	fmt.Println(headers.When)
+	fmt.Println("El slice y = ", y)
+	fmt.Println("la longitud del slice =", len(y))
+	fmt.Println("la capacidad del slice =", cap(y))
 
+	fmt.Println("\nSi usamos append para añadir elementos...")
+	fmt.Println("Append añade el/los elementos hasta que la longitud del slice al añadirlos supere la capacidad")
+	fmt.Println("que tiene el slice al momento de aplicar el append")
+
+	fmt.Println("\nAppend, duplicará la capacidad del sice al doble de la longitud que teniamos")
+	fmt.Println("antes de proceder al append.")
+
+	for i := 1; i < 6; i++ {
 		y = append(y, i)
-
-		// imprimimos el slice
+		fmt.Println("\nAñado", i)
 		fmt.Println("Slice y: ", y)
-
-		// imprimimos la longitud y la capacidad del slice según le añadimos con append i
-		fmt.Printf("longitud de y %d - capacidad de y %d - elementos %d \n", len(y), cap(y), i)
+		fmt.Printf("longitud de y = %d, capacidad de y = %d,  elementos de y = %d\n", len(y), cap(y), i)
 	}
 
 }
